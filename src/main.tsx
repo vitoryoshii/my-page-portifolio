518697e9822d87ca21572bc9fb873f21049a9b52
vitor.yoshii <vitoryoshiiprofissional@gmail.com>
2025-10-29 14:21:09 -0300

    build: Forçar purga de cache da CDN

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,12 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+// Forçar novo deploy para purgar o cache - 29/10
+
+//ReactDOM.createRoot(document.getElementById('root')!).render(
+//  <React.StrictMode>
+//    <App />
+//  </React.StrictMode>,
+//)
+
 createRoot(document.getElementById("root")!).render(<App />);
