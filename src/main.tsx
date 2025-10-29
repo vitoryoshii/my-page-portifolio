50c29be8303ffa161f28eb295560663a7efd9941
vitor.yoshii <vitoryoshiiprofissional@gmail.com>
2025-10-29 14:24:27 -0300

    build: Reverter

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,4 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-// Forçar novo deploy para purgar o cache - 29/10
-
-//ReactDOM.createRoot(document.getElementById('root')!).render(
-//  <React.StrictMode>
-//    <App />
-//  </React.StrictMode>,
-//)
-
 createRoot(document.getElementById("root")!).render(<App />);
